e7c3dfc9ef66b706a6ead0548697435b94aab974
Nathan <trijaunathan@gmail.com>
2026-02-10 23:36:11 +0100

    ex00 ex01 , 02 to finish

---
+++ piscine/starting/ex01/format_ft_time.py
@@ -0,0 +1,17 @@
+from datetime import datetime, timezone
+
+
+
+
+post_date = datetime(1970, 1, 1, tzinfo=timezone.utc)
+now = datetime.now(timezone.utc)
+
+delta = now - post_date
+#print("\n","La valeur de post_date :", post_date, ", et de now :", now,"\n","La difference delta = ", delta, "\n")
+seconds = delta.total_seconds()
+#print("\n", "seconds = ", seconds, "\n")
+
+print(f"Seconds since January 1, 1970: {seconds:,.4f} or {seconds:.2e} in scientific notation")
+
+date_str = now.strftime("%b %d %Y")
+print(date_str)
---
+++ piscine/starting/ex02/find_ft_type.py
@@ -0,0 +1,14 @@
+ft_list = ["Hello", "tata!"]
+ft_tuple = ("Hello", "toto!")
+ft_set = {"Hello", "tutu!"}
+ft_dict = {"Hello" : "titi!"}
+
+print("List :", type(ft_list))
+print("Tuple :", type(ft_tuple))
+print("Set :", type(ft_set))
+print("Dict :", type(ft_dict))
+bryan = "Brian is in the kitchen :"
+toto = "Toto is in the kitchen :"
+print(bryan, type(bryan))
+print(toto, type(toto))
+print('42')
